2178e918132fbd98b7f66ad136387f927def0fce
henderiw <wim.henderickx@gmail.com>
2022-01-17 08:19:06 +0100

    added new way to handle lists by addign aa container

--- pkg/generator/resource.go
+++ pkg/generator/resource.go
@@ -200,7 +200,7 @@ func (g *Generator) ResourceGenerator(resPath string, dynPath *gnmi.Path, e *yan
 							}
 							c := container.NewContainer(dummyYangEntry.Name, g.IsResourceBoundary(resPath), cPtr)
 							r.ContainerList = append(r.ContainerList, c)
-							cPtr.Entries = append(cPtr.Entries, g.parser.CreateContainerEntry(e, c, cPtr, containerKey))
+							cPtr.Entries = append(cPtr.Entries, g.parser.CreateContainerEntry(e, cPtr, c, containerKey))
 
 						} else {
 							// add entry to the container, containerKey allows to see if a
